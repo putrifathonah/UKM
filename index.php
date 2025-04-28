b02ecd073cda91aa81abfefc5a93a1efd73e86b4
putrifathonah <putrifathonah_2327240139@mhs.mdp.ac.id>
2025-04-29 06:55:46 +0700

    Menambahkan boostrap di index
    Tambah boostrap di index

--- index.php
+++ index.php
@@ -4,27 +4,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UKM</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-    <h3>Form Pendaftaran UKM</h3>
+    <div class="container mt-4">
+        <div class="card">
+            <div class="card-header"><strong>From Pendaftaran UKM</strong></div>
+            <form action="proses_daftar.php" method="POST">
+                <div class="card-body">
+                
+                        <div class="mb-3 mt-3">
+                            <label for="var_npm" class="form-label">NPM:</label>
+                            <input type="text" class="form-control" id="var_npm" placeholder="Input Nomor Pengenal Mahasiswa (NPM)" name="var_npm">
+                        </div>
+                        <div class="mb-3 mt-3">
+                            <label for="var_nama" class="form-label">NAMA:</label>
+                            <input type="text" class="form-control" id="var_nama" placeholder="Input Nama Lengkap Mahasiswa" name="var_nama">
+                        </div>
+                        <div class="mb-3">
+                            <label for="var_ukm" class="form-label">Pilihan UKM:</label>
+                            <select name="var_ukm" class="form-select">
+                                <option>UKM Programming</option>
+                                <option>UKM Band</option>
+                                <option>UKM Futsal</option>
+                                <option>UKM Basket</option>
+                                <option>UKM Taekwondo</option>
+                            </select>
+                        </div>
+                </div>
 
-    <form action="proses_daftar.php" method="POST">
-        <input type="text" name="var_npm" placeholder="Input NPM"/>
-        <br/><br/>
-        <input type="text" name="var_nama" placeholder="Input Nama"/>
-        <br/><br/>
-        Pilihan UKM :
-        <select name="var_ukm">
-            <option>UKM Programming</option>
-            <option>UKM Band</option>
-            <option>UKM Futsal</option>
-            <option>UKM Basket</option>
-            <option>UKM Taekwondo</option>
-        </select>
-        <br><br>
-        <input type="submit" value="Daftra"/>
-        &nbsp;
-        <input type="reset" value="Batal"/>
-    </form>
+                <div class="card-footer">
+                    <input type="submit" class="btn btn-dark">
+                    <input type="Reset" class="btn btn-danger">
+                </div>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
